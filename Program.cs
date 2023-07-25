724c00d41c4025333fd3421bf69ea98b34a6427e
Aleksey <kukoba.alexey@gmail.com>
2023-07-25 07:53:00 +0300

    finish

--- Program.cs
+++ Program.cs
@@ -2,4 +2,30 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6]-> 0
 
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
 
+int sum = 0;
+for (int i = 0; i < size; i+=2)
+sum = sum + numbers[i];
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
